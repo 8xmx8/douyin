ae4b6e33867c60d26d20f7594b0258fcf3e0d5f2
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-08-28 22:44:59 +0800

    添加文件创建函数参数注释

--- cmd/start.go
+++ cmd/start.go
@@ -36,6 +36,20 @@ func start() {
 		Args: args,
 		Env:  os.Environ(),
 	}
+	/*
+		O_RDONLY 打开只读文件
+		O_WRONLY 打开只写文件
+		O_RDWR 打开既可以读取又可以写入文件
+		O_APPEND 写入文件时将数据追加到文件尾部
+		O_CREATE 如果文件不存在，则创建一个新的文件
+		0o666：表示文件权限的八进制数。0o666 表示文件所有者、所属组和其他用户都具有读写权限。
+	*/
+	/*
+		在八进制表示法中，0o 前缀表示八进制数。数字 766 对应了文件权限 rw-rw-rw-。
+		7 表示所有者（owner）具有读取、写入和执行权限。
+		6 表示所属组（group）具有读取和写入权限。
+		6 表示其他用户（others）具有读取和写入权限。
+	*/
 	stdout, err := os.OpenFile(filepath.Join(flags.ExPath, "data", "start.log"), os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0o666)
 	if err != nil {
 		log.Fatal(os.Getpid(), ": 无法打开启动日志文件:", err)
