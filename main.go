94619c4e0263687bc881dae29676277a7218665d
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-09-02 08:22:03 +0800

    file: 对文件操作提供说明

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import "github.com/Godvictory/douyin/cmd"
+import (
+	"github.com/Godvictory/douyin/cmd"
+)
 
 func main() {
-	// trace.Start(os.Stderr)
-	// defer trace.Stop()
+	//f, err := os.Create("trace.out")
+	//if err != nil {
+	//	log.Fatalf("failed to create trace output file: %v", err)
+	//}
+	//defer func() {
+	//	f.Close()
+	//}()
+	//// 2. trace绑定文件句柄
+	//if err := trace.Start(f); err != nil {
+	//	log.Fatalf("failed to start trace: %v", err)
+	//}
+	//defer trace.Stop()
 	cmd.Execute()
 }
