23b3873c5e428dde3a2dea76be10d17fa9198c47
God_victory <128019352+Godvictory@users.noreply.github.com>
2023-09-04 12:20:48 +0800

    添加测试开发环境

--- cmd/flags/flags.go
+++ cmd/flags/flags.go
@@ -5,6 +5,7 @@ var (
 	Debug   bool   // Debug 模式
 	Dev     bool   // 开发环境
 	Pro     bool   // 生产环境
+	Tst     bool   // 测试环境
 	LogStd  bool   // 是否开始控制台输出
 	ExPath  string // 可执行文件路径
 	Memory  bool   // 是否为内存数据库
